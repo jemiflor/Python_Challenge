ab496146dbc25aa0e2696c17099621c85e256ac4
Jemima <jemiflor@gmail.com>
2021-05-08 00:35:52 -0500

    create folder structures for python challenge home work

---
+++ PyBank/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 00:20:00 2021
+
+@author: jemif
+"""
+
---
+++ PyPoll/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 00:21:46 2021
+
+@author: jemif
+"""
+
